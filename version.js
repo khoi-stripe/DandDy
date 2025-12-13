9dc4892f220ff600c96d97674b905e3c3e02779a
Khoi Uong <khoi@stripe.com>
2025-12-13 16:16:54 -0500

    Bump version to 2.3.6
    Committed-By-Agent: cursor

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 // Global version information for DandDy apps
 // Bump this in one place whenever you cut a new release.
-window.DANDDY_VERSION = '2.3.5';
+window.DANDDY_VERSION = '2.3.6';
 window.DANDDY_BACKEND_VERSION = '1.0.0';
 
 
